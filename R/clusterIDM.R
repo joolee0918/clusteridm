98a05269e94732a0cf4d3338865849ac8823c5e1
Jooyoung LEE <joolee0918@gmail.com>
2019-06-24 21:42:45 -0400

    127

--- R/clusterIDM.R
+++ R/clusterIDM.R
@@ -131,9 +131,9 @@ if(design == 1){
 
   }
   }
-print(Y.R[438,])
-print(data.R[438,])
-print(numDeriv::grad(NloglikR_pch, x=pairle$par[-1],  cut_F = cut, Y_R = as.matrix(Y.R[438,]), X_R = as.matrix(data.R[438,]), fgau = gauleg.f))
+print(Y.R[127,])
+print(data.R[127,])
+print(numDeriv::grad(NloglikR_pch, x=pairle$par[-1],  cut_F = cut, Y_R = as.matrix(Y.R[127,]), X_R = as.matrix(data.R[127,]), fgau = gauleg.f))
 score_r <- matrix(0, nrow=nr, ncol=2)
 if(no.death == FALSE) score_r <- sapply(1:nr, function(i) numDeriv::grad(loglikR_pch, x=pairle$par,  cut_F = cut, Y_R = as.matrix(Y.R[i,]), X_R = as.matrix(data.R[i,]), LAM03R = LAM03.R[i], cutR = cut.R[i], fgau = gauleg.f))
 else{
