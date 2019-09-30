94c27f3917d2ecae4c2249a41f9288ed46936579
Jooyoung LEE <joolee0918@gmail.com>
2019-09-30 14:09:54 -0400

    lam12

--- R/pair_logL.R
+++ R/pair_logL.R
@@ -13,17 +13,15 @@ pair.logL <-	function( par, Y.fam, X.fam,  Y.proband, X.proband, Y.R, X.R, Y.S,
   theta <- ifelse(lam03$Age.f <13 & lam03$Age.f >=9 , exp(par[3]), theta)
   theta <- ifelse( lam03$Age.f >=13 , exp(par[4]), theta)
 
-  lam12 <- lam03
-  lam12$rate <- lam12$rate*theta
-
+  lam12 <- lam03$rate*theta
   nr <- length(cut.R)
   LAM03.R <-  lapply(1:nr, function(j) sapply(1:length(R.fR[[j]]), function(i) lam03[lam03$Year.f==R.fR[[j]][i] & lam03$Age.f==A.fR[[j]][i], ]$rate))
-  LAM12.R <-  lapply(1:nr, function(j) sapply(1:length(R.fR[[j]]), function(i) lam12[lam03$Year.f==R.fR[[j]][i] & lam03$Age.f==A.fR[[j]][i], ]$rate))
+  LAM12.R <-  lapply(1:nr, function(j) sapply(1:length(R.fR[[j]]), function(i) lam12[lam03$Year.f==R.fR[[j]][i] & lam03$Age.f==A.fR[[j]][i]]))
 
   if(!is.null(cut.S)){
     ns <- length(cut.S)
     LAM03.S <-  lapply(1:ns, function(j) sapply(1:length(R.fS[[j]]), function(i) lam03[lam03$Year.f==R.fS[[j]][i] & lam03$Age.f==A.fS[[j]][i], ]$rate))
-    LAM12.S <-  lapply(1:ns, function(j) sapply(1:length(R.fS[[j]]), function(i) lam12[lam03$Year.f==R.fS[[j]][i] & lam03$Age.f==A.fS[[j]][i], ]$rate))
+    LAM12.S <-  lapply(1:ns, function(j) sapply(1:length(R.fS[[j]]), function(i) lam12[lam03$Year.f==R.fS[[j]][i] & lam03$Age.f==A.fS[[j]][i]]))
   }
 
   if(is.null(G)) res <- loglikFD2_pch(par, theta, Y.fam, X.fam, as.matrix(Y.proband), as.matrix(X.proband),  Age, Cal, cut, lam03, gauleg.f, utils::combn)
@@ -53,12 +51,10 @@ loglikR_pch_R <- function(par, cut_F, Y_R, X_R,  R.fR, A.fR, cutR, fgau){
   theta <- ifelse(lam03$Age.f <13 & lam03$Age.f >=9 , exp(par[3]), theta)
   theta <- ifelse( lam03$Age.f >=13 , exp(par[4]), theta)
 
-  lam12 <- lam13
-  lam12$rate <- lam12$rate*theta
-
+  lam12 <- lam03$rate*theta
   LAM03.R <- LAM12.R <- list()
   LAM03.R[[1]] <- sapply(1:length(R.fR), function(i) lam03[lam03$Year.f==R.fR[i] & lam03$Age.f==A.fR[i], ]$rate)
-  LAM12.R[[1]] <- sapply(1:length(R.fR), function(i) lam12[lam03$Year.f==R.fR[i] & lam03$Age.f==A.fR[i], ]$rate)
+  LAM12.R[[1]] <- sapply(1:length(R.fR), function(i) lam12[lam03$Year.f==R.fR[i] & lam03$Age.f==A.fR[i]])
 
 
   res <- loglikR_pch(par,  theta, cut_F, Y_R, X_R,  LAM03.R[1], LAM12.R[1], cutR, fgau)
@@ -72,12 +68,10 @@ loglikR_pch_gene_R <- function(par, cut_F, Y_R, X_R,  R.fR, A.fR, cutR, fgau){
   theta <- ifelse(lam03$Age.f <13 & lam03$Age.f >=9 , exp(par[3]), theta)
   theta <- ifelse( lam03$Age.f >=13 , exp(par[4]), theta)
 
-  lam12 <- lam13
-  lam12$rate <- lam12$rate*theta
-
+  lam12 <- lam03$rate*theta
   LAM03.R <- LAM12.R <- list()
   LAM03.R[[1]] <- sapply(1:length(R.fR), function(i) lam03[lam03$Year.f==R.fR[i] & lam03$Age.f==A.fR[i], ]$rate)
-  LAM12.R[[1]] <- sapply(1:length(R.fR), function(i) lam12[lam03$Year.f==R.fR[i] & lam03$Age.f==A.fR[i], ]$rate)
+  LAM12.R[[1]] <- sapply(1:length(R.fR), function(i) lam12[lam03$Year.f==R.fR[i] & lam03$Age.f==A.fR[i]])
 
 
   res <- loglikR_pch_gene(par,  theta, cut_F, Y_R, X_R,  LAM03.R[1], LAM12.R[1], cutR, fgau)
@@ -91,12 +85,11 @@ loglikS_pch_R <- function(par, cut_F, Y_S, X_S,  R.fS, A.fS, cutS, fgau){
   theta <- ifelse(lam03$Age.f <13 & lam03$Age.f >=9 , exp(par[3]), theta)
   theta <- ifelse( lam03$Age.f >=13 , exp(par[4]), theta)
 
-  lam12 <- lam13
-  lam12$rate <- lam12$rate*theta
+  lam12 <- lam03$rate*theta
 
   LAM03.S <- LAM12.S <- list()
   LAM03.S[[1]] <- sapply(1:length(R.fS), function(i) lam03[lam03$Year.f==R.fS[i] & lam03$Age.f==A.fS[i], ]$rate)
-  LAM12.S[[1]] <- sapply(1:length(R.fS), function(i) lam12[lam03$Year.f==R.fS[i] & lam03$Age.f==A.fS[i], ]$rate)
+  LAM12.S[[1]] <- sapply(1:length(R.fS), function(i) lam12[lam03$Year.f==R.fS[i] & lam03$Age.f==A.fS[i]])
 
 
   res <- loglikS_pch( par, theta, cut_F,  Y_S, LAM03.S[1], LAM12.S[1], cutS, fgau)
@@ -111,12 +104,10 @@ loglikS_pch_gene_R <- function(par, cut_F, Y_S, X_S,  R.fS, A.fS, cutS, fgau){
   theta <- ifelse(lam03$Age.f <13 & lam03$Age.f >=9 , exp(par[3]), theta)
   theta <- ifelse( lam03$Age.f >=13 , exp(par[4]), theta)
 
-  lam12 <- lam13
-  lam12$rate <- lam12$rate*theta
-
+  lam12 <- lam03$rate*theta
   LAM03.S <- LAM12.S <- list()
   LAM03.S[[1]] <- sapply(1:length(R.fS), function(i) lam03[lam03$Year.f==R.fS[i] & lam03$Age.f==A.fS[i], ]$rate)
-  LAM12.S[[1]] <- sapply(1:length(R.fS), function(i) lam12[lam03$Year.f==R.fS[i] & lam03$Age.f==A.fS[i], ]$rate)
+  LAM12.S[[1]] <- sapply(1:length(R.fS), function(i) lam12[lam03$Year.f==R.fS[i] & lam03$Age.f==A.fS[i]])
 
 
   res <- loglikS_pch_gene( par, theta, cut_F,  Y_S, LAM03.S[1], LAM12.S[1], cutS, fgau)
