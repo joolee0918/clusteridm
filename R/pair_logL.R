32f904758f3e0b56cc83569d316b0598040e6b44
joolee0918 <48692713+joolee0918@users.noreply.github.com>
2019-10-01 08:14:38 -0400

    Update pair_logL.R

--- R/pair_logL.R
+++ R/pair_logL.R
@@ -45,6 +45,36 @@ pair.logL <-	function( par, Y.fam, X.fam,  Y.proband, X.proband, Y.R, X.R, Y.S,
 
 
 
+loglikFD2_pch_R <- function(par, Y_F, X_F,  Y_proband, X_proband,
+                 Age, Cal,   cut_F, lam03, fgau, combn, copula){
+
+  theta <- ifelse(lam03$Age.f < 9, exp(par[2]), 0)
+  theta <- ifelse(lam03$Age.f <13 & lam03$Age.f >=9 , exp(par[3]), theta)
+  theta <- ifelse(lam03$Age.f >=13 , exp(par[4]), theta)
+
+  lam12 <- lam03$rate*theta
+  
+
+  res <- loglikFD2_pch(par, theta, Y_F, X_F, Y_proband, X_proband),  Age, Cal, cut_F, lam03, fgau, combn)
+  return(res)
+}
+                                                
+loglikFD2_pch_gene_R <- function(par, Y_F, X_F,  Y_proband, X_proband,
+                 Age, Cal,   cut_F, lam03, fgau, combn, copula){
+
+  theta <- ifelse(lam03$Age.f < 9, exp(par[2]), 0)
+  theta <- ifelse(lam03$Age.f <13 & lam03$Age.f >=9 , exp(par[3]), theta)
+  theta <- ifelse(lam03$Age.f >=13 , exp(par[4]), theta)
+
+  lam12 <- lam03$rate*theta
+  
+
+  res <- loglikFD2_pch_gene(par, theta, Y_F, X_F, Y_proband, X_proband),  Age, Cal, cut_F, lam03, fgau, combn)
+  return(res)
+}
+
+                                                
+
 loglikR_pch_R <- function(par, cut_F, Y_R, X_R,  R.fR, A.fR, cutR, fgau){
 
   theta <- ifelse(lam03$Age.f < 9, exp(par[2]), 0)
